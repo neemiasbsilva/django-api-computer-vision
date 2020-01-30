7c2e050ec8979c46e69c59852079cca4a8ac19b1
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-30 16:06:37 -0300

    Application Mvsia-challeng Computer Vision

--- pipeline/binarize_mask.py
+++ pipeline/binarize_mask.py
@@ -3,7 +3,8 @@ import numpy as np
 
 def binarize_mask(path, r, g, b, k):
     img = cv2.imread(path)
-
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    print(img[:,:,0])
     img_b = img[:, :, 0] * r + img[:, :, 1] * g + img[:, :, 2] * b
     img_b = img_b > k
     img_b = np.uint8(img_b)
--- pipeline/crop_image.py
+++ pipeline/crop_image.py
@@ -8,9 +8,17 @@ def crop_image(path, x, y, dx, dy):
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     if type(img) is np.ndarray:
 
-        img = cv2.resize(img, (512, 512))
+        # img = cv2.resize(img, (512, 512))
+        x = max(0, x)
+        y = max(0, y)
 
-        img_rectangle = img[y: y + dy, x: x + dx]
+        x = min(x, img.shape[1])
+        y = min(y, img.shape[0])
+
+        x2 = min(dx + x, img.shape[1])
+        y2 = min(y + dy, img.shape[0])
+
+        img_rectangle = img[y: y2, x: x2]
 
         cv2.imwrite(path, img_rectangle)
     else:
